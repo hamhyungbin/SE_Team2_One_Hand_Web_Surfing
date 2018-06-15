8e6538959011b1c30468df1692702dc10686a5a2
HAZIQ HAMZAH (하지크) <33246398+ajid951125@users.noreply.github.com>
2018-06-15 17:26:20 +0900

    Update popup_message.js

--- js/popup_message.js
+++ js/popup_message.js
@@ -1,3 +1,7 @@
+// If user click the chrome extension icon, this pop up page will appear
+// Then two link will be ready to be clicked
+// 1. Go to option page
+// 2. Go to gesture guidlines
 window.onload = function(){
 	var opts = chrome.extension.getURL('../pages/options_page.html');
 	var link = document.getElementById('opts_link');
